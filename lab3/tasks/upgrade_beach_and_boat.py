fd8bd9c971c122c533022d3eba373dc66c23ca3e
LvingOnEarth <lexrabxel@gmail.com>
2020-07-15 18:28:51 +0300

    Add function update in 'main'; the waves moves

--- lab3/tasks/upgrade_beach_and_boat.py
+++ lab3/tasks/upgrade_beach_and_boat.py
@@ -4,11 +4,11 @@ import math
 X_WINDOW_SIZE = 400
 Y_WINDOW_SIZE = 270
 
-x_window_size = X_WINDOW_SIZE
-y_window_size = Y_WINDOW_SIZE
+# x_window_size = X_WINDOW_SIZE
+# y_window_size = Y_WINDOW_SIZE
 
-# x_window_size = X_WINDOW_SIZE * 1.5
-# y_window_size = Y_WINDOW_SIZE * 1.5
+x_window_size = X_WINDOW_SIZE * 1.5
+y_window_size = Y_WINDOW_SIZE * 1.5
 
 # x_window_size = X_WINDOW_SIZE * 2.5
 # y_window_size = Y_WINDOW_SIZE * 2.5
@@ -17,7 +17,7 @@ x_koef_screen = x_window_size / X_WINDOW_SIZE
 y_koef_screen = y_window_size / Y_WINDOW_SIZE
 
 windowSize(x_window_size, y_window_size)
-canvasSize(x_window_size, y_window_size)
+canvasSize(x_window_size + 100, y_window_size + 100)
 
 def calculate_top_bottom_sky_sea_sand():
     y_top_bottom_arr = []
@@ -42,7 +42,9 @@ def draw_sky(y_top_bottom_arr):
     x_top, x_bottom = 0, x_window_size
     y_top, y_bottom = y_top_bottom_arr
 
-    rectangle(x_top, y_top, x_bottom, y_bottom)
+    sky = rectangle(x_top, y_top, x_bottom, y_bottom)
+
+    return sky
 
 def draw_sea(y_top_bottom_arr):
     penColor(66, 30, 224)
@@ -51,36 +53,47 @@ def draw_sea(y_top_bottom_arr):
     x_top, x_bottom = 0, x_window_size
     y_top, y_bottom = y_top_bottom_arr
 
-    rectangle(x_top, y_top, x_bottom, y_bottom)
+    sea = rectangle(x_top, y_top, x_bottom, y_bottom)
 
-def draw_sand(y_top_bottom_arr, radius = 30):
+    return sea
+
+def draw_sand(y_top_bottom_arr, radius = 30, x1 = 15):
     brushColor(239, 246, 4)
     penColor(239, 246, 4)
+
+    arr_circles = []
 
     x_top, x_bottom = 0, x_window_size
     y_top, y_bottom = y_top_bottom_arr
 
-    rectangle(x_top, y_top, x_bottom,  y_bottom)
+    sand = rectangle(x_top, y_top, x_bottom + 100,  y_bottom)
+
+    arr_circles.append(sand)
 
     count_of_sand_waves = int(x_window_size // radius)
-    x1 = 15
+    # x1 *= cycle
     y_top_circle = y_top - radius * 0.86
     y_bottom_circle = y_top + radius * 0.86
-    # x1, y1, y2 = 15, 210, 160
 
     for i in range(count_of_sand_waves):
         if i % 2 == 0:
             penColor(239, 246, 4)
             brushColor(239, 246, 4)
-            circle(x1 + i * 33, y_bottom_circle, radius)
+            circ = circle(x1 + i * 33, y_bottom_circle, radius)
         else:
             penColor(66, 30, 224)
             brushColor(66, 30, 224)
-            circle(x1 + i * 33, y_top_circle, radius)
+            circ = circle(x1 + i * 33, y_top_circle, radius)
+
+        arr_circles.append(circ)
+
+    return arr_circles
 
 def draw_sun(triangle_length = y_window_size * 0.11):
     penColor(254, 247, 25)
     brushColor(255, 247, 23)
+
+    arr_sun = []
 
     angle_of_triangle_turn = 10
 
@@ -99,8 +112,10 @@ def draw_sun(triangle_length = y_window_size * 0.11):
     y_triangle_point_3 = y_center_of_sun + half_bottom_triangle_length
 
 
-    polygon([(x_triangle_point_1, y_triangle_point_1), (x_triangle_point_2, y_triangle_point_2),
+    sunray = polygon([(x_triangle_point_1, y_triangle_point_1), (x_triangle_point_2, y_triangle_point_2),
              (x_triangle_point_3, y_triangle_point_3), (x_triangle_point_1, y_triangle_point_1)])
+
+    arr_sun.append(sunray)
 
     for i in range(36):
         alpha = (angle_of_triangle_turn + angle_of_triangle_turn * i) / 2
@@ -112,8 +127,12 @@ def draw_sun(triangle_length = y_window_size * 0.11):
         sx = s * math.cos(alpha * (math.pi / 180))
         sy = s * math.cos((90 - alpha) * (math.pi / 180))
 
-        polygon([(x_triangle_point_1 - sx, y_triangle_point_1 + sy), (x_triangle_point_2 - rx, y_triangle_point_2 - ry),
+        sunray = polygon([(x_triangle_point_1 - sx, y_triangle_point_1 + sy), (x_triangle_point_2 - rx, y_triangle_point_2 - ry),
                  (x_triangle_point_3 + sx, y_triangle_point_3 - sy), (x_triangle_point_1 - sx, y_triangle_point_1 + sy)])
+
+        arr_sun.append(sunray)
+
+    return arr_sun
 
 def calculate_point_of_cloud_circle(size_x, size_y, x, y, step):
     '''
@@ -121,7 +140,7 @@ def calculate_point_of_cloud_circle(size_x, size_y, x, y, step):
     :param size_y: size of circle in y coordinate
     :param x: x coordinate for first circle
     :param y: y coordinate for first circle
-    :param step:
+    :param step: start of next circle of a cloud
     :return: array (point_of_clouds_arr) with coordinate for all circles
     '''
     point_of_clouds_arr = []
@@ -176,14 +195,21 @@ def draw_clouds(size_x = 1, size_y = 1, x = 30, y = 25, step = 20):
     penColor(241, 241, 241)
     brushColor(255, 255, 255)
 
+    arr_clouds = []
+
     point_of_clouds_arr = calculate_point_of_cloud_circle(size_x, size_y, x, y, step)
 
     for i in point_of_clouds_arr:
-        oval(i[0], i[1], i[2], i[3])
+        circle_of_cloud = oval(i[0], i[1], i[2], i[3])
+        arr_clouds.append(circle_of_cloud)
+
+    return arr_clouds
 
 def draw_umbrella(size = 1, x = 75, y = 175):
     x *= x_koef_screen
     y *= y_koef_screen
+
+    arr_umbrella = []
 
     height = 80 * y_koef_screen
     size_of_pen = 5 / size * y_koef_screen
@@ -192,9 +218,12 @@ def draw_umbrella(size = 1, x = 75, y = 175):
     # bottom
     penSize(size_of_pen)
     penColor(228, 131, 18)
-    line(x, y, x, height // size + y)
+    line_bottom = line(x, y, x, height // size + y)
     penColor(244, 80, 80)
-    line(x, y, x, y - 25 // size * y_koef_screen)
+    line_top = line(x, y, x, y - 25 // size * y_koef_screen)
+
+    arr_umbrella.append(line_bottom)
+    arr_umbrella.append(line_top)
 
     # top
     penSize(1)
@@ -214,23 +243,32 @@ def draw_umbrella(size = 1, x = 75, y = 175):
     y3 = y
 
     # left triangle
-    polygon([(x1_left, y1), (x2_left, y2),
+    left_triangle = polygon([(x1_left, y1), (x2_left, y2),
              (x3_left, y3), (x1_left, y1)])
 
     # right triangle
-    polygon([(x1_right, y1), (x2_right, y2),
+    right_triangle = polygon([(x1_right, y1), (x2_right, y2),
              (x3_right, y3), (x1_right, y1)])
+
+    arr_umbrella.append(left_triangle)
+    arr_umbrella.append(right_triangle)
 
     # black lines on umbrella
     penColor(197, 63, 63)
     for i in range(1, 5):
-        line(x1_left + 7.5 / size * x_koef_screen * i, y1, x2_left, y2)
-        line(x1_right - 7.5 / size * x_koef_screen * i, y1, x2_right, y2)
+        left_line = line(x1_left + 7.5 / size * x_koef_screen * i, y1, x2_left, y2)
+        right_line = line(x1_right - 7.5 / size * x_koef_screen * i, y1, x2_right, y2)
 
+        arr_umbrella.append(left_line)
+        arr_umbrella.append(right_line)
+
+    return arr_umbrella
 
 def draw_boats(size = 1, x = 235, y = 135):
     penColor(165, 70, 41)
     brushColor(187, 79, 1)
+
+    arr_boat = []
 
     x *= x_koef_screen
     y *= y_koef_screen
@@ -241,19 +279,19 @@ def draw_boats(size = 1, x = 235, y = 135):
     x1 = x + x_length
     y1 = y + y_length
 
-    rectangle(x, y, x1, y1)
-    polygon([(x1, y1), (x1 + 45 * size * x_koef_screen, y),
+    mid_of_boat = rectangle(x, y, x1, y1)
+    right_of_boat = polygon([(x1, y1), (x1 + 45 * size * x_koef_screen, y),
              (x1, y), (x1, y1)])
-    arc(x - y_length, y - y_length, x + y_length, y1, 180, 270)
+    left_of_boat = arc(x - y_length, y - y_length, x + y_length, y1, 180, 270)
 
     penSize(3 * size * x_koef_screen)
     penColor(0, 0, 0)
     brushColor(255, 255, 255)
 
-    circle(x1 + 10 * size * x_koef_screen, y1 - 16.5 * size * y_koef_screen, 6 * size * x_koef_screen)
+    circ_of_boat = circle(x1 + 10 * size * x_koef_screen, y1 - 16.5 * size * y_koef_screen, 6 * size * x_koef_screen)
 
     penSize(5 * size * x_koef_screen)
-    line(x + (x1 - x) / 2 - 15 * size * x_koef_screen, y, x + (x1 - x) / 2 - 15 * size * x_koef_screen, y - 60 * size * y_koef_screen)
+    mast_of_boat = line(x + (x1 - x) / 2 - 15 * size * x_koef_screen, y, x + (x1 - x) / 2 - 15 * size * x_koef_screen, y - 60 * size * y_koef_screen)
 
     penSize(1)
     penColor(170, 173, 132)
@@ -271,31 +309,70 @@ def draw_boats(size = 1, x = 235, y = 135):
     y3_for_both = y - (y - (y - 60 * size * y_koef_screen)) / 2
 
     # bottom sail
-    polygon([(x1_for_both, y1_bottom), (x2_for_both, y2_for_both),
+    bottom_sail = polygon([(x1_for_both, y1_bottom), (x2_for_both, y2_for_both),
              (x3_for_both, y3_for_both), (x1_for_both, y1_bottom)])
 
     # top sail
-    polygon([(x1_for_both, y1_top), (x2_for_both, y2_for_both),
+    top_sail = polygon([(x1_for_both, y1_top), (x2_for_both, y2_for_both),
              (x3_for_both, y3_for_both), (x1_for_both, y1_top)])
 
+    arr_boat.append(mid_of_boat)
+    arr_boat.append(right_of_boat)
+    arr_boat.append(left_of_boat)
+    arr_boat.append(circ_of_boat)
+    arr_boat.append(mast_of_boat)
+    arr_boat.append(bottom_sail)
+    arr_boat.append(top_sail)
+
+    return arr_boat
+
+# def del_obj(arr_obj):
+#     arr = arr_obj
+#
+#     def test(arr = arr_obj):
+#         for i in range(len(arr)):
+#
+#             if isinstance(arr[i], list):
+#                 test(arr[i])
+#
+#             deleteObject(arr[i])
+#
+#     return test
+
+
+
 def main():
+    def update():
+        for i in range(len(sand)):
+            moveObjectBy(sand[i], 30, 0)
+
+        if bbox(sand[0])[0] > -1:
+            for y in range(len(sand)):
+                moveObjectBy(sand[y], -60, 0)
+
     y_top_bottom_arr = calculate_top_bottom_sky_sea_sand()
 
-    draw_sky(y_top_bottom_arr[0])
-    draw_sea(y_top_bottom_arr[1])
-    draw_sand(y_top_bottom_arr[2])
+    sky = draw_sky(y_top_bottom_arr[0])
+    sea = draw_sea(y_top_bottom_arr[1])
+    sand = draw_sand(y_top_bottom_arr[2], 30, 15)
 
-    draw_sun()
+    sun = draw_sun()
 
-    draw_clouds(1, 1, 30, 25, 20)
-    draw_clouds(1, 1.1, 150, 10, 40)
-    draw_clouds(1.4, 0.9, 50, 65, 30)
+    clouds_1 = draw_clouds(1, 1, 30, 25, 20)
+    clouds_2 = draw_clouds(1, 1.1, 150, 10, 40)
+    clouds_3 = draw_clouds(1.4, 0.9, 50, 65, 30)
 
-    draw_umbrella(size = 1, x = 75, y = 175)
-    draw_umbrella(size = 1.5, x = 175, y = 200)
+    umbrella_1 = draw_umbrella(size = 1, x = 75, y = 175)
+    umbrella_2 = draw_umbrella(size = 1.5, x = 175, y = 200)
 
-    draw_boats(size = 1, x = 235, y = 135)
-    draw_boats(size = 0.5, x = 130, y = 150)
+    boats_1 = draw_boats(size = 1, x = 235, y = 135)
+    boats_2 = draw_boats(size = 0.5, x = 130, y = 150)
+
+    onTimer(update, 250)
+
+    # delet = del_obj([sky, sea, sand, sun, clouds_1, clouds_2, clouds_3, umbrella_1, umbrella_2, boats_1, boats_2])
+
+
 
 main()
 run()
